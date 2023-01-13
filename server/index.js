d9c2ab71613ad697a9fa699760171b9b9a17b441
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-01-13 14:13:13 -0800

    possible path fix

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + 'public/index.html'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
