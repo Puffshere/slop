712f7d79569b8e866a61a05975c745a5ca8e5c77
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-01-13 01:19:07 -0800

    initial backend

---
+++ server/routes/api/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+})
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://shawn:bladex1980@cluster0.ylrym.mongodb.net/cluster0?', {
+        // useNewParser: true
+    });
+
+
+    return client.db('cluster0').collection('posts');
+
+}
+
+module.exports = router;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,40 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="golf-score">
+    <h1>Golf Scores</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Record your scores and view them in the table below.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <!-- <h3>Input Score</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <li>Week Number: <input type="text" name="week-number"/></li>
+      <li>Score: <input type="text" name="score"/></li>
+      <li><button>Submit Score</button></li>
+    </ul> -->
+    <h3>Scores Table</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Week Number</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>87</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>94</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'GolfScore',
   props: {
     msg: String
   }
@@ -55,4 +57,15 @@ li {
 a {
   color: #42b983;
 }
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+table, th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
 </style>
