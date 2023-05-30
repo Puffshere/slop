e2a3bae6da7c760d73a4f516832c0753e97e577d
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-05-30 01:21:02 -0700

    code cleanup

--- client/src/main.js
+++ client/src/main.js
@@ -1,25 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import router from "./router";
 import './index.css'
-//import { createRouter } from 'vue-router';
 
 
 createApp(App).mount('#app')
-//import VueRouter from "vue-router";
-
-// const router = createRouter({
-//     routes
-// });
-// // Routes
-// const routes = [
-//     {
-//         path: "/",
-//         component: App
-//     }
-// ];
-
-// createApp(App).use(router).mount("#app");
-
-//   export default router;
-
