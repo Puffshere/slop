711ed1795bf410f68e637f6c596a943a3d7051b7
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-06-02 14:49:55 -0700

    gets rid of app.js file

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import './index.css'
+import Home from './pages/home.vue'
 
 
-createApp(App).mount('#app')
+createApp(Home).mount('#home')
---
+++ client/src/pages/records.vue
@@ -0,0 +1,40 @@
+<template>
+    <HeaderBar />
+    <div class="golf-score">
+      <br />
+    <PostComponent />
+    </div>
+  </template>
+  
+  <script>
+  import PostComponent from './components/PostComponent.vue';
+  import HeaderBar from './components/HeaderBar.vue';
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      PostComponent,
+      HeaderBar
+  }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  
+  body{
+    background: linear-gradient(268deg, #c3c4c4, #f3f3f3);
+  }
+  
+  .golfLogo {
+    max-width: 20vh;
+  }
+  
+  </style>
