b0f61f56b143adb86b72f8af2c062d94dc8001e9
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-08-01 11:44:19 -0700

    adds after post redirect

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -79,7 +79,8 @@ export default {
         return;
       }
       await PostService.insertPost(this.text);
-      this.posts = await PostService.getPosts();
+      window.location.reload();
+      // this.posts = await PostService.getPosts();
     },
   },
 };
