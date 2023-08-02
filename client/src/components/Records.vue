cbeda617be34d5b42fc3550139c5d8aebdc94872
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-08-01 18:05:50 -0700

    adds scorecards to miramar, chula vista, and lakes

--- client/src/components/Records.vue
+++ client/src/components/Records.vue
@@ -19,7 +19,9 @@
         </thead>
         <tbody>
           <tr>
-            <td class="pointer"><a href="https://cottonwood.ezlinksgolf.com/index.html#/search?utm_source=google&utm_medium=organic">Cottonwood</a></td>
+            <td class="pointer"><a
+                href="https://cottonwood.ezlinksgolf.com/index.html#/search?utm_source=google&utm_medium=organic">Cottonwood</a>
+            </td>
             <td>96</td>
             <td>Shawn</td>
           </tr>
@@ -29,7 +31,9 @@
             <td>Jackson</td>
           </tr>
           <tr>
-            <td class="pointer"><a href="https://myffr.navyaims.com/navywest/wbwsc/navywest.wsc/search.html?module=GR&secondarycode=28&begintime=07%3A00+am&_csrf_token=fs0Y1I670L6A3M2T133L424D5M4U5M591Y70624R5K695L4G5X571J5F4P425F1J0D4T5B5S0G654G6D480A014I5C5A1N5M6H6M6J0S5Y5251646G5R4U516I0A034T5T">AB North</a></td>
+            <td class="pointer"><a
+                href="https://myffr.navyaims.com/navywest/wbwsc/navywest.wsc/search.html?module=GR&secondarycode=28&begintime=07%3A00+am&_csrf_token=fs0Y1I670L6A3M2T133L424D5M4U5M591Y70624R5K695L4G5X571J5F4P425F1J0D4T5B5S0G654G6D480A014I5C5A1N5M6H6M6J0S5Y5251646G5R4U516I0A034T5T">AB
+                North</a></td>
             <td>96</td>
             <td>Shawn</td>
           </tr>
@@ -52,27 +56,40 @@
         <tbody>
           <tr>
             <td class="pointer"><a href="https://www.chulavistagc.com/bookteetimes">Chula Vista</a></td>
+            <td class="scorecard">
+              <a @click="chula_vista" target="_blank">79</a>
+              <img :src="imageSrc" v-if="imageSrc !== ''" alt="chula vista scorecard">
+            </td>
+            <td>Shawn/Jackson</td>
+          </tr>
+          <tr>
+            <td class="pointer"><a href="https://foreupsoftware.com/index.php/booking/19103/968#/teetimes">Menifee
+                Palms</a></td>
             <td>79</td>
             <td>Shawn/Jackson</td>
           </tr>
           <tr>
-            <td class="pointer"><a href="https://foreupsoftware.com/index.php/booking/19103/968#/teetimes">Menifee Palms</a></td>
+            <td class="pointer"><a href="https://foreupsoftware.com/index.php/booking/19103/954#/teetimes">Menifee
+                Lakes</a></td>
+            <td class="scorecard">
+              <a @click="lakes" target="_blank">79</a>
+              <img :src="imageSrc" v-if="imageSrc !== ''" alt="lakes scorecard">
+            </td>
+            <td>Shawn/Jackson</td>
+          </tr>
+          <tr>
+            <td class="pointer"><a href="https://www.thegolfclubatranchocalifornia.com/book-a-tee-time/">Golf Club
+                Rancho</a></td>
             <td>79</td>
             <td>Shawn/Jackson</td>
           </tr>
           <tr>
-            <td class="pointer"><a href="https://foreupsoftware.com/index.php/booking/19103/954#/teetimes">Menifee Lakes</a></td>
-            <td>79</td>
-            <td>Shawn/Jackson</td>
-          </tr>
-          <tr>
-            <td class="pointer"><a href="https://www.thegolfclubatranchocalifornia.com/book-a-tee-time/">Golf Club Rancho</a></td>
-            <td>79</td>
-            <td>Shawn/Jackson</td>
-          </tr>
-          <tr>
-            <td class="pointer"><a href="https://www.chronogolf.com/club/miramar-memorial-golf-course#?course_id=3229&nb_holes=18">Miramar</a></td>
-            <td>81</td>
+            <td class="pointer"><a
+                href="https://www.chronogolf.com/club/miramar-memorial-golf-course#?course_id=3229&nb_holes=18">Miramar</a>
+            </td>
+            <td class="scorecard"><a @click="miramar" target="_blank">81</a>
+              <img :src="imageSrc" v-if="imageSrc !== ''" alt="miramar scorecard">
+            </td>
             <td>Shawn/Jackson</td>
           </tr>
         </tbody>
@@ -98,7 +115,9 @@
             <td>The Squad</td>
           </tr>
           <tr>
-            <td class="pointer"><a href="https://cottonwood.ezlinksgolf.com/index.html#/search?utm_source=google&utm_medium=organic">Cottonwood</a></td>
+            <td class="pointer"><a
+                href="https://cottonwood.ezlinksgolf.com/index.html#/search?utm_source=google&utm_medium=organic">Cottonwood</a>
+            </td>
             <td>73</td>
             <td>The Squad</td>
           </tr>
@@ -115,7 +134,8 @@ export default {
       error: "",
       text: "",
       posts: [],
-      password: "6980"
+      password: "6980",
+      imageSrc: ""
     };
   },
   methods: {
@@ -130,6 +150,24 @@ export default {
       // ...
       await PostService.insertPost(this.text);
       this.posts = await PostService.getPosts();
+    },
+    miramar() {
+      const imageSrc = require('../assets/scorecards/miramar.jpg');
+      window.open(imageSrc, '_blank');
+      // Use this code to open up scorecard image in table
+      // if (this.imageSrc === require('../assets/bike/bike.jpg')) {
+      //   this.imageSrc = '';
+      // } else {
+      //   this.imageSrc = require('../assets/bike/bike.jpg');
+      // }
+    },
+    lakes() {
+      const imageSrc = require('../assets/scorecards/lakes.jpg');
+      window.open(imageSrc, '_blank');
+    },
+    chula_vista() {
+      const imageSrc = require('../assets/scorecards/chula_vista.jpg');
+      window.open(imageSrc, '_blank');
     }
   }
 };
@@ -153,9 +191,18 @@ td {
   cursor: pointer;
 }
 
+.scorecard {
+  color: rgb(131, 112, 4);
+}
+
 .courseRecords {
   color: rgb(131, 112, 4);
   font-size: 25px;
   padding-bottom: 15px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
 }
 </style>
