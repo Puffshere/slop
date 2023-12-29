efbef3d6c4fc75d53341bc1d46cd51c78403704a
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-12-28 22:04:07 -0800

    adds background image to rules page

--- client/src/components/Rules.vue
+++ client/src/components/Rules.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <h1>These are your rules for 2024!</h1>
+  <div class="background">
+    <div class="textContainer">
+      <h1 class="headerText">These are your rules for 2024!</h1>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.background {
+  background: url('../assets/rules_header_background.webp') center/contain no-repeat;
+  width: 100%;
+  box-shadow: 5px 5px 30px black;
+  display: flex;
+  align-items: center;
+  margin-top: -30px;
+  background-color: black;
+}
+
+.textContainer {
+  width: 50%;
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+
+.headerText {
+  color: white;
+  font-size: 30px;
+}
+</style>
