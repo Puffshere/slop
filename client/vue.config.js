1af6fe0758b3563691936c3d1a70a0d2b5cb6169
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-12-30 09:16:15 -0800

    adds sitemap to vue.config.js file

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,10 +1,34 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
 const path = require('path');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+// You can write the paths as an array of strings or an array of objects
+const paths = [
+  {
+    path: '/',
+    lastmod: '2021-11-22',
+    priority: 1.0,
+    changefreq: 'yearly'
+  },
+  {
+    path: '/records/',
+    lastmod: '2021-11-22',
+    priority: 0.9,
+    changefreq: 'yearly'
+  },
+  {
+    path: '/rules/',
+    lastmod: '2021-11-22',
+    priority: 0.8,
+    changefreq: 'yearly'
+  }
+];
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({ base: 'https://sloppygolf.com', paths })
+    ]
+  },
   outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     proxy: {
