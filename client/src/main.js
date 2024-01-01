d6bef46bf08f069413fc52094199e0219462859e
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-12-31 21:09:31 -0800

    adds title and description for seo

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,78 @@
-import { createApp } from 'vue'
+import { createApp, nextTick } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import './index.css'
 import App from './pages/App.vue'
 import Home from './pages/Home.vue'
 import Rules from '../src/pages/Rules.vue'
 import CourseRecords from '../src/pages/CourseRecords.vue'
-import Vue from 'vue';
-import VueMeta from 'vue-meta';
 
-Vue.use(VueMeta);
 
 // Create the router instance
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home },
-        { path: '/records', component: CourseRecords },
-        { path: '/rules', component: Rules },
+        { 
+            path: '/', 
+            component: Home, 
+            meta: { 
+                title: 'Home Page - SloppyGolf.com',
+                metaTags: [
+                    {
+                        name: 'description',
+                        content: 'Website dedicated to tracking a year long golf competition between Shawn Taylor, Aaron Jackson, Bock Thomas, and Ron Butryn.'
+                    }
+                ]
+            }
+        },
+        { 
+            path: '/records', 
+            component: CourseRecords, 
+            meta: { 
+                title: 'Course Records - SloppyGolf.com',
+                metaTags: [
+                    {
+                        name: 'description',
+                        content: 'These are the current course records.'
+                    }
+                ]
+            }
+        },
+        { 
+            path: '/rules', 
+            component: Rules, 
+            meta: { 
+                title: 'Rules - SloppyGolf.com',
+                metaTags: [
+                    {
+                        name: 'description',
+                        content: 'These are the current rules for 2024.'
+                    }
+                ]
+            }
+        },
     ]
 })
+
+router.afterEach((to) => {
+    nextTick(() => {
+        // Update title
+        document.title = to.meta.title || 'SloppyGolf.com';
+
+        // Remove existing meta tags
+        Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+        // Add new meta tags
+        to.meta.metaTags?.forEach(tagDef => {
+            const tag = document.createElement('meta');
+            Object.keys(tagDef).forEach(key => {
+                tag.setAttribute(key, tagDef[key]);
+            });
+
+            tag.setAttribute('data-vue-router-controlled', '');
+            document.head.appendChild(tag);
+        });
+    });
+});
 
 const app = createApp(App)
 
--- client/src/pages/App.vue
+++ client/src/pages/App.vue
@@ -21,16 +21,6 @@ export default {
     HeaderBar,
     Footer
   },
-  metaInfo: {
-    title: 'SloppyGolf.com', // This will be the title of your page
-    meta: [
-      {
-        vmid: 'description', // This is important for avoiding duplicate meta tags
-        name: 'description',
-        content: 'This is a site used to track a yearly golf competition between Shawn Taylor, Aaron Jackson, Bock Thomas, and Ron Butryn.' // Your meta description
-      }
-    ]
-  }
 }
 </script>
   
